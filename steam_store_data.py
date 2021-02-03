9f87b93da7bf60ceeefc84a3191c85f37df1ee63
EllenSt264 <stafford.ellen@protonmail.com>
2021-02-03 14:44:41 +0000

    Retrieve more data from the Steam Store
    Retrieve the following data:
    
    * Bestselling game titles
    
    * Popular tags for each game

--- steam_store_data.py
+++ steam_store_data.py
@@ -7,8 +7,49 @@ source = requests.get(bestsellers_url)
 soup = BeautifulSoup(source.text, "html.parser")
 
 
+"""
+Sources: "https://stackoverflow.com/questions/12616912/split-an-element-with-beautifulsoup"
+"""
+
+# -------------------------------------------------- Data lists
+game_titles = []
+game_tags = []
+
+
 def scrape_data():
+
     for item in soup.select("#TopSellersRows"):
         for a in item.findAll("a", href=True):
+
+            # -------------------------------------------------- Game Titles
+
             title = a.select(".tab_item_name")
-            print(title)
+            for i in title:
+                title_string = i.string
+                game_titles.append(title_string)
+
+            # -------------------------------------------------- Game Tags
+
+            top_tags = a.select(".tab_item_top_tags span")
+
+            tags = []
+            tags_inner = []
+
+            for items in top_tags:
+                for tag in items:
+                    tag_item = tag.string
+
+                    if tag_item[0] == ",":
+                        tags_inner.append(tag_item)
+                    else:
+                        tag_item = [tag_item]
+                        tags.append(tag_item)
+
+            for i in tags_inner:
+                tags[0].append(i)
+
+            for tag in tags:
+                game_tags.append(tag)
+
+
+scrape_data()
