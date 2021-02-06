d58ab9291c6ca268c0a7cf256cf724545e427ce0
EllenSt264 <stafford.ellen@protonmail.com>
2021-02-06 20:57:03 +0000

    Update Navbar Hamburger icon and Sidenav
    Remove Materialize sidenav and replace with own custom sidenav so that users can use the hamburger icon to toggle the sidenav on and off.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     $(window).on("load", function() {
+
+        $('#hamburger').click(function(){
+            $(this).toggleClass('open');
+            $("#mobile-nav").css("width", "90%")
+
+            $(".sidenav-overlay").click(function() {
+                $("#hamburger").removeClass("open")
+                $("#mobile-nav").css("width", '0')
+            })
+
+            $('#closeSideNav').click(function() {
+                $("#hamburger").removeClass("open")
+                $("#mobile-nav").css("width", '0')
+            })
+
+            if (!$(this).hasClass('open')) {
+                $("#mobile-nav").css("width", '0')
+            }
+        });
+
         $('.sidenav').sidenav({ edge: "right" });
+        
         $('.carousel.carousel-slider').carousel({
             fullWidth: true, 
             indicators: true,
