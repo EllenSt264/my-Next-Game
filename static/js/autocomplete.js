74cd2cdf2867a28f42449652b31ff35a321caeb4
EllenSt264 <stafford.ellen@protonmail.com>
2021-04-10 08:30:32 +0100

    Fix console error for '_renderItem' in Jquery-UI
    Fix 'Uncaught TypeError: Cannot set property "_renderItem" of undefined' in jquery-min.js and scripts.js.
    
    Move the JS code that handles JQuery UI autocomplete to a separate JS file and only load the file when needed, in order to fix this error.

---
+++ static/js/autocomplete.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    /* --------------------------------------- JQuery UI Initialization */
+
+    /*
+        This code is based of the following sources:
+        * "https://stackoverflow.com/questions/2909077/autocomplete-disallow-free-text-entry"
+        * "https://www.encodedna.com/jquery/how-to-highlight-input-words-using-jqueryui-autocomplete-widget.htm"
+        * "https://www.encodedna.com/2013/08/jquery-autocomplete-dropdown-list-on-focus.htm"
+    */
+
+    /* Autocomplete */
+
+    $('#query').autocomplete({
+        source: gameData,   // Grabs game titles from data.js file
+        scroll: true,
+        // Disallow user input to only allow input that exists within the source data
+        change: function (event, ui) {
+            let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+            let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+            if (ui.item == null) {
+                $('#query').css(classInvalid);
+                event.currentTarget.value = '';
+                event.currentTarget.focus();
+            }
+            else {
+                $('#query').css(classValid);
+            }
+        }
+    // Highlight results on hover/focus
+    }).focus(function() {
+        $(this).autocomplete("search", "");
+    // Highlight input characters
+    }).data("ui-autocomplete")._renderItem = function( ul, item ) {
+        let txt = String(item.value).replace(new RegExp(this.term, "gi"),"<span class='highlight'>$&</span>");
+        return $("<li></li>")
+            .data("ui-autocomplete-item", item)
+            .append("<a>" + txt + "</a>")
+            .appendTo(ul);
+    };
+})
